122f64e87a7512f26b070e077472dee13a060d82
sebuszqo <sekn.kn@gmail.com>
2025-05-31 17:44:30 +0200

    change API on fronted side as well

--- slp-frontend/src/helpers/inspectionApi.tsx
+++ slp-frontend/src/helpers/inspectionApi.tsx
@@ -2,22 +2,22 @@ import axios from "axios"
 import {backendUrl, Header} from "../utils/urls"
 import {Inspection} from "../utils/types";
 
-const url = "inspection/"
+const url = "inspections/"
 
 const getAllInspection = () => {
-    return axios.get(backendUrl + url + "list", Header())
+    return axios.get(backendUrl + url, Header()); // GET /inspections
 }
 
 const updateInspection = (item: Inspection) => {
-    return axios.put(backendUrl + url + 'update', item, Header());
+    return axios.put(backendUrl + url + item.id, item, Header()); // PUT /inspections/:id
 }
 
 const addInspection = (item: Inspection) => {
-    return axios.post(backendUrl + url + "save", item, Header());
+    return axios.post(backendUrl + url, item, Header()); // POST /inspections
 }
 
 const deleteInspection = (id: number | null) => {
-    return axios.delete(backendUrl + url + `delete/${id}`, Header());
+    return axios.delete(backendUrl + url + id, Header()); // DELETE /inspections/:id
 }
 
 export {
--- slp-frontend/src/helpers/userApi.tsx
+++ slp-frontend/src/helpers/userApi.tsx
@@ -4,26 +4,28 @@ import { LoginData, RegisterData, ChangePasswordPayload } from "../utils/types";
 
 const url = "users/"
 
+
 export const loginRequest = (data: LoginData) => {
-    return axios.post(backendUrl + url + "login", data, Header());
+    return axios.post(backendUrl + url + "login", data, Header())  // POST /users/login - bez JWT
 }
 
+
 export const registerRequest = (data: RegisterData) => {
-    return axios.post(backendUrl + url + "register", data, Header())
+    return axios.post(backendUrl + url, data, Header())  // POST /users - rejestracja wymaga JWT
 }
 
 export const changePassword = (data: ChangePasswordPayload) => {
-    return axios.post(backendUrl + url + "change-password", data, Header())
+    return axios.post(backendUrl + url + "change-password", data, Header())  // POST /users/change-password
 }
 
 export const getUsersData = () => {
-   return axios.get(backendUrl + url, Header()) 
+    return axios.get(backendUrl + url, Header())  // GET /users, JWT + admin role
 }
 
-export const deleteUserByEmail = (email:String) => {
-    return axios.delete(backendUrl + url + `${email}`, Header());
+export const deleteUserByEmail = (email: string) => {
+    return axios.delete(backendUrl + url + email, Header())  // DELETE /users/:email, JWT + admin role
 }
 
-export const changePasswordForAdmin = (email:String, data:ChangePasswordPayload) => {
-    return axios.post(backendUrl + url + `change-password/${email}`, data, Header());
+export const changePasswordForAdmin = (email: string, data: ChangePasswordPayload) => {
+    return axios.post(backendUrl + url + `change-password/${email}`, data, Header())  // POST /users/change-password/:email, JWT + admin role
 }
