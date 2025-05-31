1fe5b23ccaef8703eb4b280901a59210b3bed4b7
Sebastian Knap <sknap@splunk.com>
2025-05-31 13:16:27 +0200

    change init sql to create new assortment_indications and assortment_id_seq

--- db-init/init.sql
+++ db-init/init.sql
@@ -4,3 +4,16 @@ ALTER TABLE users
     ALTER COLUMN id SET DEFAULT nextval('users_id_seq');
 
 SELECT setval('users_id_seq', COALESCE((SELECT MAX(id) FROM users), 1));
+
+
+ALTER TABLE assortment_indications
+DROP CONSTRAINT fk7aiabke38hkwv0ai7bmvfqtcu;
+
+ALTER TABLE assortment_indications
+    ADD CONSTRAINT fk7aiabke38hkwv0ai7bmvfqtcu FOREIGN KEY (assortment_id)
+        REFERENCES assortment(id) ON DELETE CASCADE;
+
+CREATE SEQUENCE IF NOT EXISTS assortment_id_seq;
+
+ALTER TABLE assortment ALTER COLUMN id SET DEFAULT nextval('assortment_id_seq');
+SELECT setval('assortment_id_seq', COALESCE((SELECT MAX(id) FROM assortment), 1));
